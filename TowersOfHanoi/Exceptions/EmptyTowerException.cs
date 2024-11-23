ca79ab9707840bc6a25c998d58cda7a3a803e7f1
Dejmenek <dejmen14567@gmail.com>
2024-11-24 00:34:11 +0100

    Add custom exceptions for Tower operations

---
+++ TowersOfHanoi/Exceptions/EmptyTowerException.cs
@@ -0,0 +1,9 @@
+﻿namespace TowersOfHanoi.Exceptions;
+public class EmptyTowerException : Exception
+{
+    public EmptyTowerException() { }
+
+    public EmptyTowerException(string message) : base(message) { }
+
+    public EmptyTowerException(string message, Exception inner) : base(message, inner) { }
+}
---
+++ TowersOfHanoi/Exceptions/LargerOnSmallerElementException.cs
@@ -0,0 +1,9 @@
+﻿namespace TowersOfHanoi.Exceptions;
+public class LargerOnSmallerElementException : Exception
+{
+    public LargerOnSmallerElementException() { }
+
+    public LargerOnSmallerElementException(string message) : base(message) { }
+
+    public LargerOnSmallerElementException(string message, Exception inner) : base(message, inner) { }
+}
