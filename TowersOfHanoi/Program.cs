08f31034ced65cd599ecd4b484f47f4214f7c400
Dejmenek <dejmen14567@gmail.com>
2024-11-24 00:37:02 +0100

    Add entry point Program.cs

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,11 @@
+﻿using TowersOfHanoi;
+
+internal class Program
+{
+    private static void Main()
+    {
+        GameManager gameManager = new GameManager();
+
+        gameManager.Run();
+    }
+}
