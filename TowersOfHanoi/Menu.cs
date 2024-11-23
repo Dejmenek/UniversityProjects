cfb142f7154c77cb63f5d8d7a456775bdbba21b6
Dejmenek <dejmen14567@gmail.com>
2024-11-24 00:36:01 +0100

    Add Menu class for user interactions

---
+++ TowersOfHanoi/Menu.cs
@@ -0,0 +1,26 @@
+﻿using Spectre.Console;
+using TowersOfHanoi.Enums;
+
+namespace TowersOfHanoi;
+public class Menu
+{
+    public void ShowMenu()
+    {
+        AnsiConsole.Write(
+            new FigletText("TOWERS OF HANOI")
+            .LeftJustified()
+            .Color(Color.Red)
+        );
+        AnsiConsole.Write("1. Start a new game\n2. View Instructions\n3. Exit\n");
+    }
+
+    public MenuOptions GetMenuChoice()
+    {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<MenuOptions>()
+                .Title("Select an option")
+                .AddChoices(Enum.GetValues<MenuOptions>())
+        );
+    }
+
+}
