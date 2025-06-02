8b202d5b88aa6f933800f30e9697877a96d5ff41
Dejmenek <dejmen14567@gmail.com>
2025-06-02 22:04:50 +0200

    add constants

--- DataWarehouse/etl/extract.py
+++ DataWarehouse/etl/extract.py
@@ -1,25 +1,11 @@
 import pandas
 
-dtype_dict = {
-    "Passenger ID": "object",
-    "First Name": "object",
-    "Last Name": "object",
-    "Gender": "category",
-    "Age": "Int64",
-    "Nationality": "object",
-    "Airport Name": "object",
-    "Airport Country Code": "object",
-    "Country Name": "object",
-    "Airport Continent": "category",
-    "Continents": "category",
-    "Arrival Airport": "object",
-    "Pilot Name": "object",
-    "Flight Status": "category"
-}
+from constants import DTYPE_DICT
+
 
 def load_csv(file_path: str) -> pandas.DataFrame:
     try:
-        df = pandas.read_csv(file_path, dtype=dtype_dict, parse_dates=["Departure Date"])
+        df = pandas.read_csv(file_path, dtype=DTYPE_DICT)
         return df
     except FileNotFoundError:
         print(f"[ERROR] Plik nie został znaleziony: {file_path}")
