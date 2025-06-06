b474ba934a67c9a9ae5b02db304973ba4ca64f5b
Dejmenek <dejmen14567@gmail.com>
2025-06-06 14:50:56 +0200

    add transform_flight_data function in the main file

--- DataWarehouse/etl/main.py
+++ DataWarehouse/etl/main.py
@@ -1,9 +1,14 @@
+from constants import FLIGHTS_CSV_PATH
+from transform import transform_flight_data
 from extract import load_csv
 
-FLIGHTS_CSV_PATH = "data/dataset.csv"
+
 
 def main():
+    print("Loading data from csv file")
     df = load_csv(FLIGHTS_CSV_PATH)
+    print("Transforming the flights data")
+    transform_flight_data(df)
 
 if __name__ == "__main__":
     main()
